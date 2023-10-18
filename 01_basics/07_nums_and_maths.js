000cb6bd801d9fa9f9810cae452e30e0b33c6de2
amitroyriku <141666806+amitroyriku@users.noreply.github.com>
2023-10-18 02:06:21 +0000

    nums_and_maths

---
+++ 01_basics/07_nums_and_maths.js
@@ -0,0 +1,40 @@
+const score = 400
+console.log(score); //400
+
+const balance = new Number(100)
+console.log(balance); //[Number: 100]
+
+console.log(balance.toString().length);
+
+//The toFixed() method is typically used
+//with numbers to format them with a specific number of decimal places
+console.log(balance.toFixed(1)); // 100.0
+
+const otherNumber = 123.8966
+
+
+//The toPrecision() method is used to format
+//a number to a specified total number of digits (precision).
+console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++ Maths +++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6)); //5
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
